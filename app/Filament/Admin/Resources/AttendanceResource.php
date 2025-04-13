c86727aef4af4a51128afc5586fd52d73300831a
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-04-13 17:48:13 +0530

    health monitering

--- app/Filament/Admin/Resources/AttendanceResource.php
+++ app/Filament/Admin/Resources/AttendanceResource.php
@@ -19,7 +19,7 @@ class AttendanceResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?string $navigationGroup = 'Attendance';
+    protected static ?string $navigationGroup = 'User';
 
     public static function form(Form $form): Form
     {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,21 @@ use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 //
 use Spatie\Health\Facades\Health;
-use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\CacheCheck;
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
+use Spatie\Health\Checks\Checks\CpuLoadCheck;
+use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
 use Spatie\Health\Checks\Checks\DebugModeCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+// use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
+use Spatie\Health\Checks\Checks\QueueCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
+use Spatie\Health\Checks\Checks\PingCheck;
+use Spatie\Health\Checks\Checks\RedisCheck;
+use Spatie\Health\Checks\Checks\HttpCheck;
+use Spatie\Health\Checks\Checks\OptimizedAppCheck;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -47,6 +59,22 @@ class AppServiceProvider extends ServiceProvider
             OptimizedAppCheck::new(),
             DebugModeCheck::new(),
             EnvironmentCheck::new(),
+
+            // database
+            DatabaseCheck::new(),
+            DatabaseConnectionCountCheck::new('mysql'),
+            DatabaseSizeCheck::new(),
+
+            // Queue Cache Schedule
+            QueueCheck::new(),
+            ScheduleCheck::new(),
+            CacheCheck::new(),
+            // ping
+            PingCheck::new()->url('erp.srcspatna.com'),
+            // HttpCheck::new()->url('https://your-api.com/health')->name('API Health'),
+
+            // Disk
+            UsedDiskSpaceCheck::new()->warnWhenUsedSpaceIsAbovePercentage(70),
         ]);
     }
 }
