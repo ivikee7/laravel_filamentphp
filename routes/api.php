51531146285e5cadb8be6e9010f2bf75f322a737
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-04-16 21:51:41 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,4 @@
 use App\Http\Controllers\Api\WebsiteEnquiryController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/website-enquiries', [WebsiteEnquiryController::class, 'store']);
-
+Route::match(array('GET', 'POST'), '/website-enquiries', [WebsiteEnquiryController::class, 'store']);
