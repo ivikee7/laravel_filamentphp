7c76fb3ebb26e8d7bc5ca5d3086a805b0778b3d5
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-04-22 12:05:12 +0530

    Update WebhookController.php

--- app/Http/Controllers/WhatsApp/WebhookController.php
+++ app/Http/Controllers/WhatsApp/WebhookController.php
@@ -18,21 +18,16 @@ class WebhookController extends Controller
 
     public function verify(Request $request)
     {
-        // $mode = $request->get('hub_mode');
-        // $token = $request->get('hub_verify_token');
-        // $challenge = $request->get('hub_challenge');
-
-        $mode = '';
-        $token = '';
-        $challenge = '';
-
-        Log::info('Webhook verify:', '');
+        $mode = $request->get('hub_mode');
+        $token = $request->get('hub_verify_token');
+        $challenge = $request->get('hub_challenge');
 
         $provider = WhatsAppProvider::where('verify_token', 'secret_whatsapp_token_srcspatna_com')->first();
 
+        Log::info('Webhook verify:', $provider);
+
+
         if ($mode === 'subscribe' && $provider) {
-            Log::info('Webhook verify:', $challenge);
-            Log::info('Webhook provider:', $provider);
             return response($challenge, 200);
         }
 
