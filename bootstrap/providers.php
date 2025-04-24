00e7f1111d7f841a61320221d059737b3e8bf18f
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-04-24 09:35:14 +0530

    Update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    // Other Service Providers...
+    App\Providers\AuthServiceProvider::class,
 ];
