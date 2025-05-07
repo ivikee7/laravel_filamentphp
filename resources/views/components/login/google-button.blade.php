eb743506e07362a94d1a3185c5bb7461fa582a4e
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-05-07 21:45:33 +0530

    Update google-button.blade.php

--- resources/views/components/login/google-button.blade.php
+++ resources/views/components/login/google-button.blade.php
@@ -1,11 +1,11 @@
 <div class="flex items-center justify-center mt-4">
-    <a href="{{ route('google.login') }}" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-wider hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
-        {{--  You can use an SVG icon here  --}}
-        <svg class="mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path fill="#EA4335"
-                d="M23.78 10.47h-1.06v-3.28c0-.9-.38-1.53-1.1-1.53h-3.44v-5.62h-4.32v5.62h-3.44c-.72 0-1.1.64-1.1 1.53v3.28h-1.06c-.15 0-.27.1-.27.23v3.83c0 .13.12.23.27.23h1.06v10.84c0 .6.44 1.03 1.1 1.03h3.44v-10.84h4.32v10.84h3.44c.65 0 1.1-.43 1.1-1.03v-10.84h1.06c.15 0 .23-.1.23-.23v-3.83c.03-.13-.1-.23-.22-.23z" />
-            <path fill="#FBBB00"
-                d="M12 15.8c-2.33 0-4.22-1.56-4.22-3.5 0-1.94 1.89-3.5 4.22-3.5 1.22 0 2.3.45 2.93 1.18v-4.1h4.32v12.73h-4.32v-3.58c-.63.74-1.71 1.18-2.93 1.18z" />
+    <a href="{{ route('google.login') }}"
+       class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-sm text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 w-full justify-center dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:hover:bg-gray-700">
+        <svg class="mr-2 h-5 w-5" viewBox="0 0 36 36">
+            <path d="M15.963 13.441c-.881 0-1.599.715-1.599 1.595v2.71h-2.62v-4.29H9.41v11.48h2.333v-2.62h2.62v2.62h4.226c.865 0 1.572-.71 1.572-1.57v-7.3c.001-.88-.703-1.595-1.57-1.595z" fill="#fbbc05"/>
+            <path d="M3.63 18.001a12.36 12.36 0 0 1 8.46-8.016l.316.012a12.18 12.18 0 0 1 6.257 1.917v-6.314h4.931v19.118h-4.931v-7.454a12.33 12.33 0 0 0-11.493 0v7.454H3.63V18.001z" fill="#4285f4"/>
+            <path d="M18 28.8c4.324 0 7.844-3.168 7.844-7.072 0-3.904-3.52-7.072-7.844-7.072-4.324 0-7.844 3.168-7.844 7.072 0 3.904 3.52 7.072 7.844 7.072z" fill="#188038"/>
+            <path d="M18 10.93c1.08 0 2.079.398 2.883 1.058l3.79-3.657c-2.46-2.37-5.697-3.801-9.573-3.801-7.984 0-14.479 5.895-14.479 13.19 0 7.294 6.495 13.19 14.479 13.19 3.876 0 7.113-1.431 9.573-3.885l-3.79-3.68c-.804.66-1.803 1.058-2.883 1.058-4.324 0-7.844-3.168-7.844-7.072 0-3.904 3.52-7.072 7.844-7.072z" fill="#ea4335"/>
         </svg>
         Login with Google
     </a>
