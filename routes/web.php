f17e392f9cb9ccf7d33f7021ea99e632ba1e869e
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-06-20 08:30:48 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -72,40 +72,39 @@ Route::get('/print-id-cards', function (\Illuminate\Http\Request $request) {
 })->name('print.id_cards');
 
 
-// Route::get('/generate-student-qrs', function () {
-//     // Get all student users.
-//     $students = User::role('Student')
-//         ->where('is_active', true)
-//         ->where('id', '>=', 2490)
-//         ->get();
+ Route::get('/generate-student-qrs', function () {
+     // Get all student users.
+     $students = User::role('Student')
+         ->where('is_active', true)
+         ->where('id', '>=', 2671)
+         ->get();
 
-//     if ($students->isEmpty()) {
-//         return response()->json(['error' => 'No students found.'], 404); // Handle the case where no students are found.
-//     }
+     if ($students->isEmpty()) {
+         return response()->json(['error' => 'No students found.'], 404); // Handle the case where no students are found.
+     }
 
-//     $results = []; // Array to store the URLs of the generated QR codes.
+     $results = []; // Array to store the URLs of the generated QR codes.
+
+     foreach ($students as $student) {
+         // Generate the data string for each student.  Customize this as needed.
+         $data = "https://erp.srcspatna.com/admin/id-cards/" . $student->id;
+
+         // Generate the QR code as a PNG
+         $qrCodePng = QrCode::format('png')->size(200)->generate($data);
 
 
-//     foreach ($students as $student) {
-//         // Generate the data string for each student.  Customize this as needed.
-//         $data = "https://erp.srcspatna.com/admin/id-cards/" . $student->id;
+         // Generate a unique filename for each QR code image.
+         $filename = $student->id . '.png';
 
-//         // Generate the QR code as a PNG
-//         $qrCodePng = QrCode::format('png')->size(200)->generate($data);
+         // Store the QR code PNG in the storage/qrcode directory
+         Storage::disk('local')->put('qrcode/' . $filename, $qrCodePng);
+         $url = Storage::url('qrcode/' . $filename);
 
+         $results[] = [
+             'student_id' => $student->id,
+             'url' => $url,
+         ];
+     }
 
-//         // Generate a unique filename for each QR code image.
-//         $filename = $student->id . '.png';
-
-//         // Store the QR code PNG in the storage/qrcode directory
-//         Storage::disk('local')->put('qrcode/' . $filename, $qrCodePng);
-//         $url = Storage::url('qrcode/' . $filename);
-
-//         $results[] = [
-//             'student_id' => $student->id,
-//             'url' => $url,
-//         ];
-//     }
-
-//     return response()->json($results); // Return an array of URLs, each associated with a student.
-// });
+     return response()->json($results); // Return an array of URLs, each associated with a student.
+ });
