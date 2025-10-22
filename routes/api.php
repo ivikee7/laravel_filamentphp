3ed4719931bf0dbc2631bafd28453c6aa71843af
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-10-22 15:50:46 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\WebsiteEnquiryController;
 use App\Http\Controllers\WhatsApp\WebhookController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::match(array('GET', 'POST'), '/website-enquiries', [WebsiteEnquiryController::class, 'store']);
 
 
