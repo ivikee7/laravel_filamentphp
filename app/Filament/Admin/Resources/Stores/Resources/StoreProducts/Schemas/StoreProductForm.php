da50b4adaeeaa294b2443270cb901524695b38fe
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-12-01 16:02:54 +0530

    Update StoreProductForm.php

--- app/Filament/Admin/Resources/Stores/Resources/StoreProducts/Schemas/StoreProductForm.php
+++ app/Filament/Admin/Resources/Stores/Resources/StoreProducts/Schemas/StoreProductForm.php
@@ -5,7 +5,12 @@ namespace App\Filament\Admin\Resources\Stores\Resources\StoreProducts\Schemas;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
+use Illuminate\Database\Eloquent\Builder;
 
 class StoreProductForm
 {
@@ -14,9 +19,21 @@ class StoreProductForm
         return $schema
             ->components([
                 Select::make('academic_year_id')
-                    ->relationship('academicYear', 'name'),
+                    ->label('Academic Year')
+                    ->relationship('academicYear', 'name')
+                    ->required()
+                    ->reactive() // Required to trigger reactivity on dependent fields
+                    ->afterStateUpdated(function (Set $set) {
+                        $set('student_class_id', null);
+                    }),
                 Select::make('class_id')
-                    ->relationship('class', 'name'),
+                    ->label('Class')
+                    ->relationship('studentClass', 'name', modifyQueryUsing: function (Builder $query, Get $get): Builder {
+                        return $query->when($get('academic_year_id'), fn(Builder $q) => $q->where('academic_year_id', $get('academic_year_id')));
+                    })
+                    ->reactive() // Make this field reactive to trigger an update on the section field.
+                    ->required()
+                    ->visible(fn(Get $get) => filled($get('academic_year_id'))), // Hide until an academic year is selected
                 TextInput::make('name')
                     ->required()
                     ->maxLength(100),
