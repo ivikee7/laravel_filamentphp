03973029835e8920e376b1f2ce50f238315b0be7
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-12-02 11:11:56 +0530

    Update RegistrationWidget.php

--- app/Filament/Admin/Resources/Registrations/Widgets/RegistrationWidget.php
+++ app/Filament/Admin/Resources/Registrations/Widgets/RegistrationWidget.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\DB;
 
 class RegistrationWidget extends ChartWidget
 {
-    protected ?string $heading = 'Registration Widget';
+    protected ?string $heading = 'Registration';
 
     protected int|string|array $columnSpan = 'full';
 
