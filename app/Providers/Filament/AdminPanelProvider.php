072ee714465518efb7d2f8fc311eefc55084de0d
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-12-03 11:18:24 +0530

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -44,8 +44,6 @@ class AdminPanelProvider extends PanelProvider
             ->widgets([
                 AccountWidget::class,
 //                FilamentInfoWidget::class,
-                \App\Filament\Admin\Resources\Registrations\Widgets\RegistrationAdmissionComparisonWidget::class,
-                \App\Filament\Admin\Resources\Registrations\Widgets\RegistrationEnquiryComparisonWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
