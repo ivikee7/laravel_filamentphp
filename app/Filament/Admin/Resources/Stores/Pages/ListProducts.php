15e5e6c7630c109d473e52b81824e56ad56085ec
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-12-04 18:52:33 +0530

    Update ListProducts.php

--- app/Filament/Admin/Resources/Stores/Pages/ListProducts.php
+++ app/Filament/Admin/Resources/Stores/Pages/ListProducts.php
@@ -33,7 +33,7 @@ class ListProducts extends Page implements HasTable, HasForms
     {
         return [
             Action::make('store')->url(StoreResource::getUrl('view', ['record' => $this->record])),
-            Action::make('list-products')->url(StoreResource::getUrl('list-products', ['record' => $this->record])),
+            Action::make('list-invoices')->url(StoreResource::getUrl('list-invoices', ['record' => $this->record])),
             Action::make('list-transactions')->url(StoreResource::getUrl('list-transactions', ['record' => $this->record])),
             Action::make('list-students')->url(StoreResource::getUrl('list-students', ['record' => $this->record])),
         ];
