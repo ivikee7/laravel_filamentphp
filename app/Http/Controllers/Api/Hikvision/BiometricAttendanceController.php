f961104975ffeaef00584ce5d26baeb775db9daf
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-12-19 13:21:12 +0530

    Update BiometricAttendanceController.php

--- app/Http/Controllers/Api/Hikvision/BiometricAttendanceController.php
+++ app/Http/Controllers/Api/Hikvision/BiometricAttendanceController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Attendance;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class BiometricAttendanceController extends Controller
 {
