f0cc9bb6c832aea008597888446084a5dfa9c78c
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2025-12-26 12:49:04 +0530

    Update ListStudentProduct.php

--- app/Filament/Admin/Resources/Stores/Pages/ListStudentProduct.php
+++ app/Filament/Admin/Resources/Stores/Pages/ListStudentProduct.php
@@ -81,12 +81,12 @@ class ListStudentProduct extends Page implements HasTable
             ->where('user_id', $this->student->id);
     }
 
-//    public function getInvoiceItemsQuery(): EloquentBuilder
-//    {
-//        return StoreInvoiceItem::with('storeInvoice')
-//            ->withWhereRelation('storeInvoice.store_id', $this->record->id)
-//            ->where('storeInvoice.user_id', $this->student->id);
-//    }
+    public function getInvoiceItemsQuery(): EloquentBuilder
+    {
+        return StoreInvoiceItem::with('storeInvoice')
+            ->withWhereRelation('storeInvoice.store_id', $this->record->id)
+            ->where('storeInvoice.user_id', $this->student->id);
+    }
 
     public function getProductTableQuery(): EloquentBuilder
     {
@@ -95,7 +95,7 @@ class ListStudentProduct extends Page implements HasTable
             ->where('academic_year_id', $this->academicYearId)
             ->where('is_multiple', true)
             ->orWhereNotIn('id', $this->getCartItemsQuery()->pluck('store_product_id'))
-//            ->whereNotIn('id', $this->getInvoiceItemsQuery()->pluck('store_product_id'))
+            ->whereNotIn('id', $this->getInvoiceItemsQuery()->pluck('store_product_id'))
             ;
     }
 
