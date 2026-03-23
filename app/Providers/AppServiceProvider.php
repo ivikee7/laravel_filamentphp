1cf1a53f708fe309add98d6d7bbe4c99f52c8cc8
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2026-03-23 10:49:57 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Filament\Forms\Components\Field;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,10 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Field::configureUsing(function (Field $field): void {
-            $field->extraInputAttributes([
-                'autocomplete' => 'off',
-            ]);
-        });
+        //
     }
 }
