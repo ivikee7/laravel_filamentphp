92c7b092c4f4a1ae0a396258235814fbdb3ab280
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2026-03-23 10:45:04 +0530

    disable  autocomplete

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Forms\Components\Field;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Field::configureUsing(function (Field $field): void {
+            $field->extraInputAttributes([
+                'autocomplete' => 'off',
+            ]);
+        });
     }
 }
