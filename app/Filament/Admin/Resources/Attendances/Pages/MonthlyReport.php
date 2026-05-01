a7c304e14effe80e94c3395382e6d5ddd56ddc66
Vikee Kumar <80277577+ivikee7@users.noreply.github.com>
2026-05-01 09:41:55 +0530

    Update MonthlyReport.php

--- app/Filament/Admin/Resources/Attendances/Pages/MonthlyReport.php
+++ app/Filament/Admin/Resources/Attendances/Pages/MonthlyReport.php
@@ -156,8 +156,8 @@ class MonthlyReport extends Page implements HasTable
                         }
 
                         $print_data = [
-                            'start_date' => Carbon::parse($this->fromDate)->format('YY-m-d'),
-                            'end_date' => Carbon::parse($this->toDate)->format('YY-m-d'),
+                            'start_date' => Carbon::parse($this->fromDate)->format('d-m-Y'),
+                            'end_date' => Carbon::parse($this->toDate)->format('d-m-Y'),
                             'columns' => $columns,
                             'column_keys' => $columnKeys,
                             'records' => $recordsArray,
